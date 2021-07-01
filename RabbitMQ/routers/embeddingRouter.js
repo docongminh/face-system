822b6085f7e8caaf4f2f66325910a674ca13ff2a
minhdc <minhdc@fabbi.io>
2021-07-01 19:12:37 +0700

    updated

--- RabbitMQ/routers/embeddingRouter.js
+++ RabbitMQ/routers/embeddingRouter.js
@@ -2,10 +2,12 @@ const router = require('express').Router();
 const embedding = require('../controllers/embeddingController');
 
 // create embedding
-router.post('/', (req, res) => {
+router.post('/', function(req, res) {
     return embedding.create(req, res);
 });
 // get all embedding
 router.get('/', (req, res) => {
     return embedding.getAll(req, res);
 });
+
+module.exports = router;
