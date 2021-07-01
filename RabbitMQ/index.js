b1580301627621d5efe7ed5d96220e7d7857634b
minhdc <minhdc@fabbi.io>
2021-07-01 19:11:34 +0700

    body-parser

--- RabbitMQ/index.js
+++ RabbitMQ/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const rabbitMQ = require('./services/rabbitmqService')
 const dbconnection = require('./utils/db')
 app = express()
 const api_version = '/api/v1';
 const port = 5000;
-app.use(`${api_version}/hello`, require('./routers/helloworld'));
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json({limit: "100mb"}));
+app.use(`${api_version}/hello`, require('./routers/helloworld'));
+app.use(`${api_version}/extract`, require('./routers/embeddingRouter'));
 app.listen(port, () => {
     console.log(`Listing at: ${port}`);
 });
