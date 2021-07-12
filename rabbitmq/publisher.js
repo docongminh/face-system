08fcf8859ab7449f699871c22bc85fc6ac6f5d3a
minhdc <minhdc@fabbi.io>
2021-07-12 18:05:12 +0700

    inprogress

--- rabbitmq/publisher.js
+++ rabbitmq/publisher.js
@@ -2,21 +2,28 @@ const amqp = require('amqplib');
 const exchangeName = "face";
 const exchangeType = 'direct';
 const config = require('../config');
-const host = 'localhost:5672'
 const {connectMQ, createChannel} = require('./utils');
 const tempStorage = {};
 
-async function send(exchange, host, rout_key, message, {callback = undefined ,  params = {} }){
-    tempStorage[rout_key] = {callback, params};
-    const connection = connectMQ(host);
+async function send(connection, exchange, key, message, {callback = undefined ,  params = {} }){
+    /**
+     * TODO LATER
+     */
+    tempStorage[key] = {callback, params};
     const channel = createChannel(connection);
-    channel.pushlish(exchange, key, new Buffer(message))
+    channel.assertExchange(exchange, 'direct',{
+        durable: true
+    });
+    channel.publish(exchange, key, new Buffer.from(message))
     console.log(" [x] Sending %s: '%s'", key, message);
     setTimeout(
-        function(){
-            connection.close();
-        }
-        5000;
+        console.log("Error publish message to Queue...!"),
+        5000
     );
+}
+
+async function listenResponse(connection){
+    const channel = createChannel(connection);
+    channel.assertQueue()
 }
 
