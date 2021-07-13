03d2a169cbec99fe7779cf1716795cd25c8ed485
minhdc <minhdc@fabbi.io>
2021-07-14 00:13:15 +0700

    modify st

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const rabbitMQ = require('./rabbitmq/setup');
 const dbconnection = require('./utils/db');
+const directExchange = require('./rabbitmq/publisher');
 
 //
 app = express();
@@ -10,11 +11,11 @@ const port = 5000;
 
 app.use(bodyParser.urlencoded({ extended:true }) );
 app.use(bodyParser.json({limit: "100mb"}));
-app.use(`${api_version}/hello`, require('./helloworld/hello_world'));
 app.use(`${api_version}/face`, require('./face_services/fs_router'));
 app.listen(port, () => {
     console.log(`Listing at: ${port}`);
 });
 
 dbconnection.connectDB();
-rabbitMQ.connect();
+// rabbitMQ.connect();
+directExchange.run();
