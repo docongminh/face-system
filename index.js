b38423b3cd8e09149be82bdb3bac305c53c2635d
minhdc <minhdc@fabbi.io>
2021-08-01 17:13:15 +0700

    search & insert api

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const port = 5000;
 
 app.use(bodyParser.urlencoded({ extended:true }) );
 app.use(bodyParser.json({limit: "100mb"}));
-app.use(`${api_version}/face`, require('./face_services/fs_router'));
+app.use(`${api_version}/search`, require('./face_services/fs_router'));
+app.use(`${api_version}/insert`, require('./face_services/fs_router'));
 app.listen(port, () => {
     console.log(`Listing at: ${port}`);
 });
